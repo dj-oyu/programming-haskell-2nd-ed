f1ac95d2d4a2509a2dc0fbfc91c3731abce7fd72
dj-oyu <uyokumissile@gmail.com>
2024-11-02 17:53:29 +0900

    テストフレームワークHSpecを導入

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,54 +1,61 @@
 import Chapter8
-import Control.Exception (throw)
-
-ch8tests :: Bool
-ch8tests = do
-    let p1 = And (Var 'A') (Not (Var 'A'))
-    let p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
-    let p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
-    let p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
-
-    let b = bools 4
-    let subs = substs p2
-
-    let v = value (Add (Val 1) (Val 2))
-
-    and [
-        vars p1 == ['A', 'A'],
-        vars p2 == ['A', 'B', 'A'],
-        vars p3 == ['A', 'A', 'B'],
-        vars p4 == ['A', 'A', 'B', 'B'],
-        isTaut p1 == False,
-        isTaut p2 == True,
-        isTaut p3 == False,
-        isTaut p4 == True,
-        b == [[False, False, False, False],
-              [False, False, False, True],
-              [False, False, True,  False],
-              [False, False, True,  True],
-              [False, True,  False, False],
-              [False, True,  False, True],
-              [False, True,  True,  False],
-              [False, True,  True,  True],
-              [True,  False, False, False],
-              [True,  False, False, True],
-              [True,  False, True,  False],
-              [True,  False, True,  True],
-              [True,  True,  False, False],
-              [True,  True,  False, True],
-              [True,  True,  True,  False],
-              [True,  True,  True,  True]],
-        subs == [[('A', False), ('B', False)],
-                 [('A', False), ('B', True)],
-                 [('A', True),  ('B', False)],
-                 [('A', True),  ('B', True)]],
-        v == 3
-        ]
-
+import Test.Hspec
 
 main :: IO ()
-main = do
-        if ch8tests then putStrLn "Chapter8 tests passed" else throw (userError "Chapter8 tests failed")
-        if findTest 2 == "two" then putStrLn "findTest passed" else throw (userError "findTest failed")
-        putStrLn "All tests passed"
+main = hspec $ do
+    describe "Chapter8 tests" $ do
+        it "vars function" $ do
+            let p1 = And (Var 'A') (Not (Var 'A'))
+            let p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+            let p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+            let p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+            vars p1 `shouldBe` ['A', 'A']
+            vars p2 `shouldBe` ['A', 'B', 'A']
+            vars p3 `shouldBe` ['A', 'A', 'B']
+            vars p4 `shouldBe` ['A', 'A', 'B', 'B']
+
+        it "isTaut function" $ do
+            let p1 = And (Var 'A') (Not (Var 'A'))
+            let p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+            let p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+            let p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+            isTaut p1 `shouldBe` False
+            isTaut p2 `shouldBe` True
+            isTaut p3 `shouldBe` False
+            isTaut p4 `shouldBe` True
+
+        it "bools function" $ do
+            let b = bools 4
+            b `shouldBe` [[False, False, False, False],
+                          [False, False, False, True],
+                          [False, False, True,  False],
+                          [False, False, True,  True],
+                          [False, True,  False, False],
+                          [False, True,  False, True],
+                          [False, True,  True,  False],
+                          [False, True,  True,  True],
+                          [True,  False, False, False],
+                          [True,  False, False, True],
+                          [True,  False, True,  False],
+                          [True,  False, True,  True],
+                          [True,  True,  False, False],
+                          [True,  True,  False, True],
+                          [True,  True,  True,  False],
+                          [True,  True,  True,  True]]
+
+        it "substs function" $ do
+            let p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+            let subs = substs p2
+            subs `shouldBe` [[('A', False), ('B', False)],
+                             [('A', False), ('B', True)],
+                             [('A', True),  ('B', False)],
+                             [('A', True),  ('B', True)]]
+
+        it "value function" $ do
+            let v = value (Add (Val 1) (Val 2))
+            v `shouldBe` 3
+
+    describe "findTest function" $ do
+        it "returns 'two' for input 2" $ do
+            findTest 2 `shouldBe` "two"
 
